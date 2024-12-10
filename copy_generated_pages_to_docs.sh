b5af6da686389dd31597c12773a0b55646a13b9c
Volker Benders <39129892+CWACoderWithAttitude@users.noreply.github.com>
2024-12-10 23:09:44 +0100

    updated

---
+++ copy_generated_pages_to_docs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#cp -r ~/.config/joplin-desktop/plugin-data/ylc395.pagesPublisher/output/* ./docs
+SRC=~/.config/joplin-desktop/plugin-data/ylc395.pagesPublisher/output
+DST=./docs
+#cp -r ~/.config/joplin-desktop/plugin-data/ylc395.pagesPublisher/output/* ./docs
+cp -r $SRC/* $DST
+
